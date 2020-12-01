b075ac4c08961e7925f81028ada2f9dfb57330ae
lshachar <13820621+lshachar@users.noreply.github.com>
2020-12-01 15:43:16 +0200

    Serial read to console

---
+++ WiiBalanceWalker/SerialRead.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SerialReadTest
+{
+    class SerialRead
+    {
+        public static void SerialReadInit()     //static void Main(string[] args)
+        {
+            Console.WriteLine("Serial read init");
+            SerialPort port = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            while (true)
+            {
+                Console.WriteLine(port.ReadLine());
+            }
+
+        }
+    }
+}
